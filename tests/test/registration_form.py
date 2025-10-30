e985034e5904675cc23693365b114c1db13382b9
Denzel <butmanovich@yandex.ru>
2025-10-30 09:35:51 +0300

    second commit

---
+++ tests/test/registration_form.py
@@ -0,0 +1,72 @@
+from selene import have
+
+
+class RegistrationFormPage:
+
+    def __init__(self, in_browser):
+        self.browser = in_browser
+
+    def open(self, value):
+        self.browser.open(value)
+
+    def fill_name(self, name):
+        self.browser.element("#firstName").type(name)
+
+    def fill_surname(self, name):
+        self.browser.element("#lastName").type(name)
+
+    def fill_email(self, email):
+        self.browser.element("#userEmail").type(email)
+
+    def choose_gender(self, value):
+        self.browser.element(f'[for="gender-radio-{value}"]').click()
+
+    def fill_number(self, value):
+        self.browser.element("#userNumber").type(value)
+
+    def fill_date_of_birth(self, year, month, day):
+        self.browser.element('#dateOfBirthInput').click()
+        self.browser.element('.react-datepicker__month-select').type(month)
+        self.browser.element('.react-datepicker__year-select').type(year)
+        self.browser.element(
+            f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
+        ).click()
+
+    def choose_subject(self, value):
+        self.browser.element("#subjectsInput").type(value).press_enter()
+
+    def choose_hobby(self):
+        self.browser.element('label[for="hobbies-checkbox-1"]').click()
+
+    def choose_photo(self, value):
+        self.browser.element("#uploadPicture").set_value(os.path.abspath(f'./img/{value}'))
+
+    def fill_address(self, value):
+        self.browser.element('#currentAddress').type(value)
+
+    def choose_state(self, value):
+        self.browser.element('#state').click()
+        self.browser.all('div[class*="option"]').element_by(have.text(value)).click()
+
+    def choose_city(self, value):
+        self.browser.element('#city').click()
+        self.browser.all('div[class*="option"]').element_by(have.text(value)).click()
+
+    def submit_form(self):
+        self.browser.element('#submit').click()
+
+    def should_registered_student_with(self, full_name, email, gender, number, date_of_birth,
+                                       subject, hobby, photo, address, state, city):
+        self.browser.element('.table').all('td').even.should(have.texts([
+            full_name,
+            email,
+            gender,
+            number,
+            date_of_birth,
+            subject,
+            hobby,
+            photo,
+            address,
+            state + ' ' + city
+        ]
+        ))
---
+++ tests/test/test_registration_form.py
@@ -0,0 +1,42 @@
+import allure
+from registration_form import RegistrationFormPage
+
+
+def test_sending_form(options_for_browser):
+    browser = options_for_browser
+
+    register_page = RegistrationFormPage(options_for_browser)
+    with allure.step("open registration page"):
+        register_page.open("https://demoqa.com/automation-practice-form")
+    with allure.step("filling personal information"):
+        register_page.fill_name("Daniil")
+        register_page.fill_surname("Zhuravlev")
+        register_page.fill_email("butmanovich@yandex.ru")
+        register_page.fill_number("3123131232")
+        register_page.choose_gender(1)
+        register_page.fill_date_of_birth('2002', 'May', '11')
+    with allure.step("filling other info"):
+        register_page.choose_subject("Computer Science")
+        register_page.choose_hobby()
+    # register_page.choose_photo('123123.png')
+    with allure.step("filling address information"):
+        register_page.fill_address('Moskva, street 10')
+        register_page.choose_state('NCR')
+        register_page.choose_city('Delhi')
+    with allure.step("sending form"):
+        register_page.submit_form()
+    with allure.step("check if registration form is working"):
+        register_page.should_registered_student_with(
+            "Daniil Zhuravlev",
+            "butmanovich@yandex.ru",
+            "Male",
+            "3123131232",
+            "11 May,2002",
+            'Computer Science',
+            'Sports',
+            '',
+            'Moskva, street 10',
+            'NCR',
+            'Delhi'
+        )
+        browser.element('#closeLargeModal').click()
