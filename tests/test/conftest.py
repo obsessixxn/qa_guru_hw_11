7215f6b94c1c28ee72074f2840538b6c474b008a
Denzel <butmanovich@yandex.ru>
2025-10-30 09:32:25 +0300

    first commit

---
+++ tests/test/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from selene import browser, Config, Browser
+from selenium import webdriver
+from utils import attach
+
+
+@pytest.fixture()
+def options_for_browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser = Browser(
+        Config(
+            driver=driver,
+            timeout=10,
+            window_width=1920,
+            window_height=1080,
+        )
+    )
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVideo": True,
+            "enableVNS": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    browser.driver.set_window_size(1920, 1080)
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
